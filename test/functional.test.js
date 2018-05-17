db78f9e35d47c1c5f2564665c3846d4b9e3b1c05
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-05-17 16:16:42 +0200

    feat() Basic filter challenge

--- test/functional.test.js
+++ test/functional.test.js
@@ -1,6 +1,7 @@
 const expect = require('chai').expect;
 const repeat = require('./../higherorderfunctions');
 const doubleAll = require('./../basicmap');
+const getShortMessages = require('./../basicfilter');
 /* global define, it, describe, before, beforeEach, afterEach, after */
 
 describe('Higher order functions', () => {
@@ -11,10 +12,28 @@ describe('Higher order functions', () => {
     expect(a).to.be.equal(4);
   });
 });
-
 describe('Basic Map', () => {
   it('SHould double all numbers', () => {
     const result = doubleAll([1, 2, 3, 4]);
     expect([2, 4, 6, 8]).to.be.deep.equal(result);
+  });
+});
+describe('Basic Filter', () => {
+  it('Should return one message', () => {
+    const messages = [
+      {message: 'Este es el mensaje que sale'},
+      {message: 'Este mensaje no sale porque es mucho mas largo de las 50 lineas que tenemos que filtrar asi que ya no va a salir'}
+    ];
+    const resultMessages = getShortMessages(messages);
+    expect(resultMessages).to.be.deep.equal([ messages[0].message ]);
+  });
+  it('Should return two message', () => {
+    const messages = [
+      {message: 'Este es el mensaje que sale'},
+      {message: 'Este es el mensaje que sale'},
+      {message: 'Este mensaje no sale porque es mucho mas largo de las 50 lineas que tenemos que filtrar asi que ya no va a salir'}
+    ];
+    const resultMessages = getShortMessages(messages);
+    expect(resultMessages).to.be.deep.equal([messages[0].message, messages[1].message]);
   });
 });
